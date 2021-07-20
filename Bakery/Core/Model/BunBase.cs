a4c022c59a8794d72dda46185277062a7ad6293a
Anton Klepikov <aklepikov@oneincsystems.com>
2021-07-20 20:51:35 +0500

    Add base class

---
+++ Bakery/Core/Model/BunBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bakery.Core.Model
+{
+    public abstract class BunBase
+    {
+        private decimal _price;
+
+        public decimal Price 
+        { 
+            get
+            {
+                int hoursElapsed = (DateTime.Now - SellUntil).Hours;
+
+                return _price - hoursElapsed * _price * (Constants.ReductionPercentage / 100);
+            }
+            set
+            {
+                _price = value <= Constants.MaxPrice ? value : Constants.MaxPrice;
+            }
+        }
+
+        public DateTime SellUntil { get; set; }
+
+        public DateTime TargetSaleTime { get; set; }
+
+        public DateTime BakeTime { get; set; }
+    }
+}
---
+++ Bakery/Core/Model/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Bakery.Core.Model
+{
+    public static class Constants
+    {
+        public const int ReductionPercentage = 2;
+        public const int MaxPrice = 100;
+    }
+}
