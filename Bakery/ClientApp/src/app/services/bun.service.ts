efde9664a17c4138ebbd4e78c4046c0469f78fa9
Anton Klepikov <aklepikov@oneincsystems.com>
2021-07-22 15:51:01 +0500

    Add frontend bun service

---
+++ Bakery/ClientApp/src/app/services/bun.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, Inject } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Bun } from '../models/bun'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+@Injectable()
+export class BunService {
+    constructor(private _httpClient: HttpClient, @Inject('BASE_URL') private _baseUrl: string)
+    {
+    }
+
+    public GetAll(): Observable<Bun[]> {
+        return this._httpClient.get(this._baseUrl + 'api/bun/getAll').pipe(
+            map((data: any) => {
+                return data.map(bun => new Bun(
+                    bun.originalPrice,
+                    bun.currentPrice,
+                    bun.sellUntil,
+                    bun.targetSaleTime,
+                    bun.bakeTime));
+            })
+        );
+    }
+}
